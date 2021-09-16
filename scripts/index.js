8c4ef72896ea1ac988aeefbce79f361d74c2eae4
Ty Maraist <tcmaraist@gmail.com>
2021-09-16 17:17:07 -0500

    generate card with user input

--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,88 @@
 //Wrappers
-const cardTemplate = document.querySelector('#cardTemplate').content.querySelector('.card');
+const editModal = document.querySelector('.modal_type_edit');
+const editForm = editModal.querySelector('.form');
+const addModal = document.querySelector('.modal_type_add');
+const addForm = addModal.querySelector('.form');
+
 const cards = document.querySelector('.cards');
-/*
-** Query Selectors
-*/
+const cardTemplate = document.querySelector('#cardTemplate').content.querySelector('.card');
 
-// profile
+
+// Buttons
 const editProfileButton = document.querySelector('.profile__edit-button');
-const profileName = document.querySelector('.profile__name')
-const profileAbout = document.querySelector('.profile__about')
+const editProfileCloseButton = editModal.querySelector('.modal__close-button');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
 
-// modal
-const modal = document.querySelector('.modal');
-const closeButton = document.querySelector('.modal__close-button');
+const addCardButton = document.querySelector('.profile__add-button');
+const addModalCloseButton = addModal.querySelector('.modal__close-button');
 
-//form
-const form = document.querySelector('.form');
-const nameInput = document.querySelector('#form-name');
-const aboutInput = document.querySelector('#form-about');
+//Form data
+const nameInput = editForm.querySelector('.form__input_type_name');
+const aboutInput = editForm.querySelector('.form__input_type_about');
+const addTitleValue = document.querySelector('.form__input_type_title');
+const addImageUrlValue = addForm.querySelector('.form__input_type_image-url');
 
 /*
 ** Functions
 */
-function openModal() {
-  modal.classList.add('modal_is-open');
-  nameInput.value = profileName.textContent;
-  aboutInput.value = profileAbout.textContent;
+function generateCards(card) {
+  const cardEl = cardTemplate.cloneNode(true);
+  cardEl.querySelector('.card__title').textContent = card.title;
+  cardEl.querySelector('.card__image').style.backgroundImage = `url(${card.image})`;
+
+  return cardEl;
 }
 
-function closeModal() {
-    modal.classList.remove('modal_is-open');
+function prefillEditForm(modalWindow) {
+  if (!modalWindow.classList.contains('modal_is-open')) {
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAbout.textContent;
+  }
 }
 
-function updateProfile(event){
+function toggleModalWindow(modalWindow) {
+  modalWindow.classList.toggle('modal_is-open');
+}
+
+function editFormSubmitHandler(event){
     event.preventDefault();
     profileName.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
 
-    closeModal();
+    toggleModalWindow(editModal);
+}
+
+function addFormSubmitHandler(event) {
+  event.preventDefault();
+  const card = {
+    title: addTitleValue.value,
+    image: addImageUrlValue.value,
+  };
+
+  const cardEl = generateCards(card);
+  cards.prepend(cardEl);
+  toggleModalWindow(addModal);
 }
 
 /*
 **  Event Listeners
 */
 
-form.addEventListener('submit', updateProfile, false);
-editProfileButton.addEventListener('click', openModal, false);
-closeButton.addEventListener('click', closeModal, false);
+editForm.addEventListener('submit', editFormSubmitHandler);
+editProfileButton.addEventListener('click', () => {
+  prefillEditForm(editModal);
+  toggleModalWindow(editModal);
+});
+editProfileCloseButton.addEventListener('click', () => toggleModalWindow(editModal));
+
+addForm.addEventListener('submit', addFormSubmitHandler);
+addCardButton.addEventListener('click', () => toggleModalWindow(addModal));
+addModalCloseButton.addEventListener('click', () => toggleModalWindow(addModal));
 
 // Actions
 initialCards.forEach((card) => {
-  // use template
-  const cardEl = cardTemplate.cloneNode(true);
-  //set the title
-  cardEl.querySelector('.card__title').textContent = card.title;
-  //set the image
-  cardEl.querySelector('.card__image').style.backgroundImage = `url(${card.image})`;
-  //append it to the list
+  cardEl = generateCards(card);
+
   cards.append(cardEl);
 });
